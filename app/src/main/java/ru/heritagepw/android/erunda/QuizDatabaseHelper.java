addc64baa214657780e474746d4df79ed30a19ff
Victor Malkov <malkov.victor@gmail.com>
2019-05-15 13:07:33 +0300

    db update

--- app/src/main/java/ru/heritagepw/android/erunda/QuizDatabaseHelper.java
+++ app/src/main/java/ru/heritagepw/android/erunda/QuizDatabaseHelper.java
@@ -14,7 +14,7 @@ import android.util.Log;
 
 public class QuizDatabaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "questions.db";
-    private static final int DB_VERSION = 10;
+    private static final int DB_VERSION = 12;
     private final Context myContext;
 
     public QuizDatabaseHelper(Context context) {
